055c6ed9904bc3489606c6b0a9bc2839601c2c41
Chad Linthicum <portableanswers@gmail.com>
2024-02-01 19:37:04 -0500

    2 of each kind are being generated and placed randomly

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -15,6 +15,9 @@ public class SceneController : MonoBehaviour
     void Start()
     {
         Vector3 startPos = originalCard.transform.position;
+
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = ShuffleArray(numbers);
         for (int i = 0; i < gridCols; i++)
         {
             for(int j = 0; j < gridRows; j++)
@@ -27,25 +30,29 @@ public class SceneController : MonoBehaviour
                 {
                     card = Instantiate(originalCard) as MemoryCard;
                 }
-                int id = Random.Range(0, images.Length);
+
+                int index = j * gridCols + i;
+                int id = numbers[index];
                 originalCard.SetCard(id, images[id]);
 
                 float posX = (offsetX * i) + startPos.x;
                 float posY = -(offsetY * j) + startPos.y;
                 card.transform.position = new Vector3(posX, posY, startPos.z);
             }
-        }
-
-
-
-       
+        }       
     }
 
-    // Update is called once per frame
-    void Update()
+    private int[] ShuffleArray (int[] numbers)
     {
-        
+        int[] newArray = numbers.Clone() as int[];
+        for (int i = 0; i <newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+        }
+        return newArray;
     }
-
     
 }
