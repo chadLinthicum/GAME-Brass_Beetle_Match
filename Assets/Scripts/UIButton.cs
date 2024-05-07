ecca30b28965f770cfa0b29a817e57d1f0bdb65b
Chad Linthicum <portableanswers@gmail.com>
2024-05-07 18:45:47 -0400

    - Added a reset button with hover effects, no reset functionality yet.

--- Assets/Scripts/UIButton.cs
+++ Assets/Scripts/UIButton.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class UIButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    public Color defaultColor = Color.white;
+    public Color highlightColor = Color.cyan;
+    public void OnMouseOver()
     {
-        
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = defaultColor;
+        }
+    }
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMouseUp()
     {
-        
+        transform.localScale = Vector3.one;
+        if(targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
     }
 }
